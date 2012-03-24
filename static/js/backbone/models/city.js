ba47befbb235036f7d98e00b120445d30c8bd3b1
Chris DeFelice <christopherdefelice@wi-design.com>
2012-03-24 16:33:44 -0400

    added auto increment to check-all id and for attr. for sibling labels to hook label click functionality on check boxes

--- static/js/backbone/models/city.js
+++ static/js/backbone/models/city.js
@@ -55,7 +55,17 @@
 			
 			.append( city_view.render().el )
 			.find('.check-all')
-			.each( function() { $(this).setupCheckboxAll( {closestWrapper: $(this).closest('ul')} ) });
+			.each( function(i) { 
+				// need to add id separate id attr. for each check-all input
+				$(this)
+				.attr('id' , 'check-all-' + ( i + 1 ))
+				// add and find sibling label
+				.add()
+				.next('label')
+				// need to add same id from sibling check all input to for attr. for silbing label
+				.attr('for', 'check-all-' + ( i + 1 ));
+				$(this).setupCheckboxAll( {closestWrapper: $(this).closest('ul')} ) 
+			});
 			
 			
 		},
