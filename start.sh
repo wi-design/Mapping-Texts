f45fb2733fcabe3dc9a766dd1c1e747cf0ab5c47
Charlie Conroy <charlieconroy300@gmail.com>
2012-04-29 15:32:58 -0400

    start script comment out slaves

--- start.sh
+++ start.sh
@@ -6,14 +6,14 @@ echo "nginx started"
 redis-server setup/redis/redis.truncated-1000.conf &
 echo "Redis master started"
 
-redis-server setup/redis/redis.slave01.truncated-1000.conf &
-echo "Redis slave01 started"
+#redis-server setup/redis/redis.slave01.truncated-1000.conf &
+#echo "Redis slave01 started"
 
-redis-server setup/redis/redis.slave02.truncated-1000.conf &
-echo "Redis slave02 started"
+#redis-server setup/redis/redis.slave02.truncated-1000.conf &
+#echo "Redis slave02 started"
 
-redis-server setup/redis/redis.slave03.truncated-1000.conf &
-echo "Redis slave03 started"
+#redis-server setup/redis/redis.slave03.truncated-1000.conf &
+#echo "Redis slave03 started"
 
 PID_FILE="server.pid"
 python server.py --port=9000 &>server.log &
